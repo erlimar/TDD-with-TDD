75be2d88adf97837333c632005ecd657a84b7ec2
jonas mikael ferreira da silva <jonas.mikael@outlook.com>
2018-01-20 10:22:42 -0200

    Criação de projeto inicial.

---
+++ TDDWithProject/TDDWithProject.Test/TesteInicial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TDDWithProject.Test
+{
+    public class TesteInicial
+    {
+        [Fact]
+        public void TesteQualquer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
