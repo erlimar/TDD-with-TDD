ba4fa5e942896a676a793695218fe7bc80050d87
Erlimar Silva Campos <erlimar@gmail.com>
2018-03-01 08:13:07 -0300

    Cria teste "Arquivo_Inexistente_Eh_Invalido()"

---
+++ src/TDDWithProject/Tester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TDDWithProject
+{
+    // TODO: Renomears
+    public class Tester
+    {
+        public bool ValidarArquivo(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/TDDWithProject.Test/TesterTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace TDDWithProject.Test
+{
+    [Trait("Target", nameof(Tester))]
+    [Trait("Module", "TDDWithProject")]
+    [Trait("Feature", "Validar Assembly .NET")]
+    public class TesterTests
+    {
+        /*
+        Cenario: Arquivo inexistente é inválido
+            Dado Que estou na linha de comando
+            E Informo o caminho para um arquivo inexistente
+            Quando Aciono a funcionalidade Validar
+            Entao O resultado exibido é FALSO ("O arquivo não existe")
+        */
+        [Fact(DisplayName = "Arquivo inexistente é inválido")]
+        public void Arquivo_Inexistente_Eh_Invalido()
+        {
+            // Arrange
+            var path = "C:\\Arquivo\\invalido.dll";
+            var sut = new Tester();
+
+            // Act
+            bool resultado = sut.ValidarArquivo(path);
+
+            // Assert
+            Assert.False(resultado);
+        }
+    }
+}
